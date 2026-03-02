f68b5f680ef308c5ca63fee557de45696211ec8f
Symqa <semkaferdi04@gmail.com>
2026-03-02 16:20:28 +0500

    lab 6

--- semestr_2/№6_Строки/a.md
+++
@@ -1,1 +0,0 @@
-![.](img/block_schem.png)
--- semestr_2/№6_Строки/main.cpp
+++ semestr_2/№6_Строки/main.cpp
@@ -6,50 +6,53 @@ using namespace std;
 
 
 int main() {
-	auto start = std::chrono::high_resolution_clock::now();
 	setlocale(LC_ALL, "ru");
 	system("chcp 1251");
 	system("cls");
 	string s;
+	cout << "Для завершения программы впишите: 0" << endl << endl;
 	cout << "Введите строку: ";
 	getline(cin, s);
-	
-	int i = 0;
-	while (i < s.length()) {
-		bool haveNumbers = false, haveLetters = false, haveSomethingElse = false;
-		int startWordIndex = i;
-		while (s[i] != ' ' && i < s.length()) {
-			unsigned char c = s[i];
-			if (c >= '0' && c <= '9') {
-				haveNumbers = true;
+	while (s != "0") {
+		auto start = std::chrono::high_resolution_clock::now();
+		int i = 0;
+		while (i < s.length()) {
+			bool haveNumbers = false, haveLetters = false, haveSomethingElse = false;
+			int startWordIndex = i;
+			while (s[i] != ' ' && i < s.length()) {
+				unsigned char c = s[i];
+				if (c >= '0' && c <= '9') {
+					haveNumbers = true;
+				}
+				else if ((c >= 192 && c <= 255) || c == 184 || c == 168) {
+					haveLetters = true;
+				}
+				else if (c >= 'A' && c <= 'z') {
+					haveLetters = true;
+				}
+				else {
+					haveSomethingElse = true;
+				}
+				i++;
 			}
-			else if (( c >= 192 && c <= 255) || c == 184 || c == 168) {
-				haveLetters = true;
-			}
-			else if (c >= 'A' && c <= 'z') {
-				haveLetters = true;
-			}
-			else {
-				haveSomethingElse = true;
+			if (!(haveNumbers && haveLetters) || (haveSomethingElse)) {
+				s.erase(startWordIndex - 1, i - startWordIndex + 1);
+				i = startWordIndex;
 			}
 			i++;
 		}
-		if (!(haveNumbers && haveLetters) || (haveSomethingElse)) {
-			s.erase(startWordIndex-1, i-startWordIndex+1);
-			i = startWordIndex;
+		if (s.length() == 0) {
+			cout << "Слов, удовлетворяющих условию не осталось :( ";
 		}
-		i++;
-	}
-	if (s.length() == 0) {
-		cout << "Слов, удовлетворяющих условию не осталось :( ";
-	}
-	else {
-		cout << "Выходная строка: " << s;
-	}
-	auto end = std::chrono::high_resolution_clock::now();
-	std::chrono::duration<double> diff = end - start;
-	std::cout << endl << "Время выполнения: " << diff.count() << " секунд" << std::endl;
-	
+		else {
+			cout << "Выходная строка: " << s;
+		}
+		auto end = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double> diff = end - start;
+		std::cout << endl << "Время выполнения: " << diff.count() << " секунд" << endl << endl;
 
+		cout << "Введите строку: ";
+		getline(cin, s);
+	}
 	return 0;
 }
